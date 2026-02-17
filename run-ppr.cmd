1a3883b271dc6296529dde0833264689674ad06f
Thomas <thomasa@yourdigitalagents.com>
2026-02-17 18:34:09 +0530

    Clear AR_EMAIL and AR_PASSWORD variables
    Removed sensitive email and password from script.

--- run-ppr.cmd
+++ run-ppr.cmd
@@ -1,6 +1,6 @@
 @echo off
-set AR_EMAIL=thomasa@yourdigitalagents.com
-set AR_PASSWORD=ThomasADas3081
+set AR_EMAIL=
+set AR_PASSWORD=
 set AR_CHROME_PROFILE=C:\PPRChrome
 set AR_SHOW_UI=1
 set AR_APPS_SCRIPT_URL=https://script.google.com/macros/s/AKfycbyiLsNiB_pPu8mKmn-yypHyGsRzIVSF9o1jZJx1BVYaPMvrGL_Gj4L7dMv5fAr9WN3s/exec
